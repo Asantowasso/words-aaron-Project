4baca9c2e3a04813ae0397f281926b7dcb10f501
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-24 22:07:54 -0400

    able to run requests with API

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 // This is where I will implement the API
 
-console.log("is it here")
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/narrow')
-  .then(res => res.json())
-  .then(data =>console.log(data))
+console.log("is it here");
+fetch("https://api.dictionaryapi.dev/api/v2/entries/en/narrow", {
+  method: "GET",
+})
+  .then(res => {return  res.json()})
+  .then((data) => console.log(data))
+  
+  // Extracting the information
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
