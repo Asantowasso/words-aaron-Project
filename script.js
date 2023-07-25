05d43b19a8c255942fd6ff3ad11459cc62a819ad
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-24 20:53:14 -0400

    able to hit API

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 // This is where I will implement the API
 
+console.log("is it here")
+fetch('https://api.dictionaryapi.dev/api/v2/entries/en/narrow')
+  .then(res => res.json())
+  .then(data =>console.log(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
