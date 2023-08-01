da70b1fe9d13afd44923e34e8ae6702a2dd7c6a1
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-31 21:14:52 -0400

    isolated names of departments

--- script.js
+++ script.js
@@ -5,14 +5,11 @@
 fetch ('https://collectionapi.metmuseum.org/public/collection/v1/departments').then(function (response) {
   return response.json();
 }).then(function (obj){
-  console.log(obj.departments[0]);
-  console.log(obj.departments[1])
-  console.log(obj.departments[8])
-  console.log(obj.departments[10])
+  console.log(obj.departments[0].displayName);
+  console.log(obj.departments[1].displayName)
+  console.log(obj.departments[8].displayName)
+  console.log(obj.departments[10].displayName)
 })
-
-
-
 
 
 
