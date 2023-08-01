b22314b59b879de783b5842e0cee5e85ec34cd41
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-31 21:42:50 -0400

    displaying text from the API on the page

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ fetch ('https://collectionapi.metmuseum.org/public/collection/v1/departments').t
   return response.json();
 }).then(function (obj){
   console.log(obj.departments[0].displayName);
-  console.log(obj.departments[1].displayName)
-  console.log(obj.departments[8].displayName)
-  console.log(obj.departments[10].displayName)
+  console.log(obj.departments[1].displayName);
+  console.log(obj.departments[8].displayName);
+  console.log(obj.departments[10].displayName);
+
+  var name1 = obj.departments[0].displayName;
+  document.querySelector(".section1").innerHTML = name1
 })
+
 
 
 
