f05ab31bbb0743b82159f6f5fc3d9e8f65d6aa94
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-31 20:41:22 -0400

    only seeing array

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
 // This is where I will implement the API
 
+fetch ('https://collectionapi.metmuseum.org/public/collection/v1/departments').then(function (response) {
+  return response.json();
+}).then(function (obj){
+  console.log(obj.departments);
+})
 
-const apiEndpoint = 'https://jsonplaceholder.typicode.com/users';
-const display = document.querySelector("#display-data")
 
-const getData = async () => {
-  const res = await fetch (apiEndpoint);
-  const data = await res.json();
-  return data
-}
 
-// getData()
 
-const displayJokes = async () => {
-  const payload  = await getData();
-  console.log(payload)
 
-  let dataDisplay = payload.map((object) => {
-    const {name, username} = object;
 
-    return `
-    <div>
-    <div class="container">
-    <p>name: ${name}</p>
-    <p>username: ${username}</p>
-    </div>
-    `
 
-  });
 
-  display.innerHTML = dataDisplay
 
-}
-displayJokes();
-  
+
 
 
 
