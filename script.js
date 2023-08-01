bdc98ae713dcdb7ca7c74b73120563ef13abdb29
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-31 20:54:54 -0400

    selected four departments

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // This is where I will implement the API
 
+//Finds info about the departments
+
 fetch ('https://collectionapi.metmuseum.org/public/collection/v1/departments').then(function (response) {
   return response.json();
 }).then(function (obj){
-  console.log(obj.departments);
+  console.log(obj.departments[0]);
+  console.log(obj.departments[1])
+  console.log(obj.departments[8])
+  console.log(obj.departments[10])
 })
+
+
+
 
 
 
