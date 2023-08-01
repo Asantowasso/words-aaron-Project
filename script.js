ea5d95a84ed5257fadbebfc7091b267af108091d
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-07-31 21:52:01 -0400

    displaying all desired sections

--- script.js
+++ script.js
@@ -2,31 +2,29 @@
 
 //Finds info about the departments
 
-fetch ('https://collectionapi.metmuseum.org/public/collection/v1/departments').then(function (response) {
-  return response.json();
-}).then(function (obj){
-  console.log(obj.departments[0].displayName);
-  console.log(obj.departments[1].displayName);
-  console.log(obj.departments[8].displayName);
-  console.log(obj.departments[10].displayName);
+fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (obj) {
+    console.log(obj.departments[0].displayName);
+    console.log(obj.departments[1].displayName);
+    console.log(obj.departments[8].displayName);
+    console.log(obj.departments[10].displayName);
 
-  var name1 = obj.departments[0].displayName;
-  document.querySelector(".section1").innerHTML = name1
-})
+    // American Decorative Arts display
+    var name1 = obj.departments[0].displayName;
+    document.querySelector(".section1").innerHTML = name1;
 
+    //Ancient Near Eastern Art display
+    var name2 = obj.departments[1].displayName;
+    document.querySelector(".section2").innerHTML = name2;
 
+    //Egyptian Art
+    var name3 = obj.departments[8].displayName;
+    document.querySelector(".section3").innerHTML = name3;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //European Sculpture and Decorative Arts
+    var name4 = obj.departments[10].displayName;
+    document.querySelector(".section4").innerHTML = name4;
+  });
