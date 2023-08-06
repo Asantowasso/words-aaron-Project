65cd1a091309e46347a5a11633d8cd45f5cd5837
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-08-06 18:59:13 -0400

    using an array to make multiple fetch requests

--- script.js
+++ script.js
@@ -29,30 +29,109 @@ fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
     document.querySelector(".section4").innerHTML = name4;
   });
 
-  // Now I want to access specific pieces 
-  
+// This function displays the objectIDs of four unique pieces from each of the feature collections
 
-  fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1|3|10|12")
+fetch(
+  "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1|3|10|12"
+)
   .then(function (response) {
-    
     return response.json();
   })
-  .then(function (obj){
-    console.log(obj.objectIDs)
-    console.log(obj.objectIDs[1000])
-    console.log(obj.objectIDs[61800])
-    console.log(obj.objectIDs[68186])
-    console.log(obj.objectIDs[33151])
+  .then(function (obj) {
+    // console.log(obj.objectIDs[1000])
+    console.log(obj.objectIDs[61800]);
+    console.log(obj.objectIDs[68186]);
+    console.log(obj.objectIDs[33151]);
+  });
 
-    var art1 = obj.objectIDs[1000];
-    document.querySelector(".item1").innerHTML = art1;
 
-    var art2 = obj.objectIDs[61800];
-    document.querySelector(".item2").innerHTML = art2;
 
-    var art3 = obj.objectIDs[68186];
-    document.querySelector(".item3").innerHTML = art3;
 
-    var art4 = obj.objectIDs[33151];
-    document.querySelector(".item4").innerHTML = art4;
-  }) 
+var req1 = "3397"
+var req2 = "323912"
+var req3 = "56766"
+
+const reqs = [req1, req2, req3]
+
+let i = 0
+while (i < reqs.length){
+  console.log(reqs[i]);
+  i++
+}
+
+fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + reqs[i])
+  .then (function (response){
+    return response.json()
+  })
+
+
+// Promise.any(reqs)
+// .then(function (response){
+//   return response.data();
+// })
+// .then(function(obj){
+//   console.log(obj)
+// })
+
+
+
+
+
+
+
+
+// var art1 = obj.objectIDs[1000];
+// document.querySelector(".item1").innerHTML = art1;
+
+// var art2 = obj.objectIDs[61800];
+// document.querySelector(".item2").innerHTML = art2;
+
+// var art3 = obj.objectIDs[68186];
+// document.querySelector(".item3").innerHTML = art3;
+
+// var art4 = obj.objectIDs[33151];
+// document.querySelector(".item4").innerHTML = art4;
+
+//retrieving information for each of the featured items
+// Item from the American Collection
+// fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/3397").then(function(response){
+//   return response.json()
+// })
+// .then(function (obj){
+//   console.log(obj)
+//   console.log(obj.title)
+//   console.log(obj.primaryImage)
+//   console.log(obj.accessionYear)
+//   console.log(obj.objectID)
+
+//   var art1 = obj.title;
+//   document.querySelector(".item1").innerHTML = art1
+// })
+
+// fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/323912").then(function(response){
+//   return response.json()
+// })
+// .then(function (obj){
+//   console.log(obj)
+//   console.log(obj.title)
+//   console.log(obj.primaryImage)
+//   console.log(obj.accessionYear)
+//   console.log(obj.objectID)
+
+//   var art2 = obj.title;
+//   document.querySelector(".item2").innerHTML = art2
+// })
+
+// fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/56766").then(function(response){
+//   return response.json()
+// })
+// .then(function (obj){
+//   console.log(obj)
+//   console.log(obj.title)
+//   console.log(obj.primaryImage)
+//   console.log(obj.accessionYear)
+//   console.log(obj.objectID)
+
+//   var art3 = obj.title;
+//   document.querySelector(".item3").innerHTML = art3
+// })
